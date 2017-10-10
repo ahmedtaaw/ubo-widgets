0db6603fe42871420dce107cd87846fb4573c2cb
ahmedtaaw <ahmedtaaw@gmail.com>
2017-10-10 18:39:33 +0200

    udate

---
+++ assets/script/script.js
@@ -0,0 +1,110 @@
+let daynumber, day, month, year, weekday;
+year = new Date().getFullYear();
+daynumber = new Date().getDate();
+weekday = new Date().getDay();
+let tomorrowweekday = new Date();
+let tomorrowdaynumber = new Date();
+let weekArray = [];
+let writeweek = [];
+$(document).ready(function () {
+
+    /****append and get days after and before */
+    writetabs(DaysAfterBeforeToday());
+    let tabCarousel = $('.tabs-carousel');
+    $('.select2').select2({ minimumResultsForSearch: Infinity });
+    /*tabCarousel.owlCarousel({
+        nav: true,
+        dots: false,
+        items: 7
+    });*/
+    let pickerlinkTodaysdate=new Date();
+    $('.datepicker').Zebra_DatePicker();
+
+
+
+});
+function writetabs(ev) {
+    let tabCarousel = $('.tabs-carousel');
+    for (let [index, value] of ev.entries()) {
+        if (index == 2) {
+            tabCarousel.append(`<li role="presentation" class="active"><a href="#tab` + index + `" aria-controls="tab` + index + `" role="tab" data-toggle="tab" aria-expanded="true">` + value + `</a></li>`);
+        } else {
+            tabCarousel.append(`<li role="presentation"><a href="#tab` + index + `" aria-controls="tab` + index + `" role="tab" data-toggle="tab">` + value + `</a></li>`);
+        }
+    }
+}
+function DaysAfterBeforeToday() {
+    for (let i = -1; i < 6; i++) {
+        tomorrowweekday.setDate(weekday + i);
+        tomorrowdaynumber.setDate(daynumber + (i - 1));
+        day5week();
+    }
+    return weekArray;
+}
+function day5week() {
+    switch (new Date().getMonth()) {
+        case 0:
+            month = "يناير";
+            break;
+        case 1:
+            month = "فبراير";
+            break;
+        case 2:
+            month = "مارس";
+            break;
+        case 3:
+            month = "ابريل";
+            break;
+        case 4:
+            month = "مايو";
+            break;
+        case 5:
+            month = "يوليو";
+            break;
+        case 6:
+            month = "يونيو";
+            break;
+        case 7:
+            month = "أغسطس";
+            break;
+        case 8:
+            month = "سبتمبر";
+            break;
+        case 9:
+            month = "أكتوبر";
+            break;
+        case 10:
+            month = "نوفمبر";
+            break;
+        case 11:
+            month = "ديسمبر";
+            break;
+
+    }
+    switch (tomorrowweekday.getDay()) {
+        case 0:
+            day = "الأحد";
+            break;
+        case 1:
+            day = "الإثنين";
+            break;
+        case 2:
+            day = "الثلاثاء";
+            break;
+        case 3:
+            day = "الاربعاء";
+            break;
+        case 4:
+            day = "الخميس";
+            break;
+        case 5:
+            day = "الجمعة";
+            break;
+        case 6:
+            day = "السبت";
+    }
+
+    weekArray.push((day + " " + tomorrowdaynumber.getDate() + " " + month));
+
+}
+
